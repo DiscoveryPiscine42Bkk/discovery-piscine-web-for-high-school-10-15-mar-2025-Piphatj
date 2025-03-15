225e33b9a044d2ad18eb99af29935c4889463c15
Piphat Narmsang <pnarmsan@z2t15c4.42bangkok.com>
2025-03-15 16:11:11 +0700

    completed!!!!!!!

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
 
 
   function addTask(task) {
-    var taskDiv = $('<div class="todo"></div>').text(task);
+    var taskDiv = $('<div class="todo"></div>').text(decodeURIComponent(task));
     
-    // Prepend it to the task list
+
     $('#ft_list').prepend(taskDiv);
 
-    // Save the tasks to cookies
+ 
     saveTasks();
   }
 
-  // Event listener to remove a task
+
   $('#ft_list').on('click', '.todo', function() {
     var confirmation = confirm("Do you want to remove this task?");
     
@@ -30,31 +30,27 @@ $(document).ready(function() {
       saveTasks();
     }
   });
-
-  // Function to save tasks in cookies
   function saveTasks() {
     var tasks = [];
     $('#ft_list .todo').each(function() {
-      tasks.push($(this).text());
+      tasks.push(encodeURIComponent($(this).text()));
     });
     
-    // Save tasks as a cookie
+   
     document.cookie = "tasks=" + JSON.stringify(tasks) + ";path=/";
   }
 
-  // Function to load tasks from cookies
   function loadTasks() {
     var cookies = document.cookie.split(';');
     var tasks = [];
 
-    // Look for the 'tasks' cookie
+
     cookies.forEach(function(cookie) {
       if (cookie.trim().startsWith("tasks=")) {
         tasks = JSON.parse(cookie.trim().substring("tasks=".length));
       }
     });
 
-    // Add each task to the list
     tasks.forEach(function(task) {
       addTask(task);
     });
